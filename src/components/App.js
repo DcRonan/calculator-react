cc7d2e3e959e79e539bb639ca853b1d649147b29
Daniel Ronan <danielconnorronan@gmail.com>
2021-02-03 17:48:49 +0000

    edit App comp'

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
 const App = () => (
   <section>
     <header>Calculator - React</header>
+    <Display result="0" />
+    <ButtonPanel />
   </section>
 );
 
