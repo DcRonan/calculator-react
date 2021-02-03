a612f0eb6dc4645169d499ff67d21c9cdfbc2378
Daniel Ronan <danielconnorronan@gmail.com>
2021-02-03 17:26:39 +0000

    delete components and install big.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,6 @@
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
-
 const App = () => (
   <section>
     <header>Calculator - React</header>
-    <Display result="0" />
-    <ButtonPanel />
   </section>
 );
 
--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import propTypes from 'prop-types';
-
-const { Button } = btn => <button type="button">{btn.key}</button>;
-
-export default Button;
-
-Button.propTypes = {
-  key: propTypes.string,
-};
