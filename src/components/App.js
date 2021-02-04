c26b8da234e953314e8f4f46c6f6884dcc8308a0
Daniel Ronan <danielconnorronan@gmail.com>
2021-02-04 15:46:23 +0000

    change App from func to class, add default states and render func

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
+import React, { Component } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <section>
-    <Display />
-    <ButtonPanel />
-  </section>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <section>
+        <Display />
+        <ButtonPanel />
+      </section>
+    );
+  }
+}
