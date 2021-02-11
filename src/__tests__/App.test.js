3df385f7bd62b358b31cfc7f2e5914ed2de35436
Daniel Ronan <danielconnorronan@gmail.com>
2021-02-11 08:54:01 +0000

    add 3 tests for App component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders calculator link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Let`s do some math!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('checks if the App component renders correctly', () => {
+  test('renders calculator link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Let`s do some math!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should render correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should display a correct number', () => {
+    render(<App />);
+    const btn = screen.getByText('5');
+    expect(btn).toBeInTheDocument();
+  });
+
+  test('should receive correct value when clicking specific buttons', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('x'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('0'));
+    fireEvent.click(screen.getByText('='));
+
+    expect(screen.getByText('520')).toBeInTheDocument();
+  });
 });
