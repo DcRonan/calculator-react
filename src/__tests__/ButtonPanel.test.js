269eee754a0b15cb1182d3dff2669f9bd06d56df
Daniel Ronan <danielconnorronan@gmail.com>
2021-02-11 08:41:29 +0000

    add test case and test for ButtonPanel

--- src/__tests__/ButtonPanel.test.js
+++ src/__tests__/ButtonPanel.test.js
@@ -1,11 +1,13 @@
 import renderer from 'react-test-renderer';
 import ButtonPanel from '../components/ButtonPanel';
 
-test('should render correctly', () => {
-  const handleClick = () => '';
+describe('checks rendering for ButtonPanel', () => {
+  test('should render correctly', () => {
+    const handleClick = () => '';
 
-  const tree = renderer
-    .create(<ButtonPanel name="AC" handleClick={handleClick} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+    const tree = renderer
+      .create(<ButtonPanel name="AC" handleClick={handleClick} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
